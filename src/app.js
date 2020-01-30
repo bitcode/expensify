de7173df88a413af4d46a46f1ea98cbe270f4193
Droz <marc.droz@RTCMACVK06VHTDG.local>
2020-01-29 19:28:24 -0600

    deleted Indecision App and used boilerplate to create Expensify App

--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-// ******************************************************************
-// **                                                              **
-// *****           Renders to Index, What?, Where?              *****
-// *****               IndecisionApp Class                      *****
-// **                                                              **
-// ******************************************************************
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+const AddExpensePage = () => <div>This is from my expense component</div>;
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+const EditExpensePage = () => <div>This is from my expense component</div>;
+
+const HelpPage = () => <div>This is from my help component</div>;
+
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+  </header>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/expense" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
